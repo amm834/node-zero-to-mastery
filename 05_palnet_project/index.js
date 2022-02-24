175fd7bd7210e54d0c645b94c1b4b8f6aa473b91
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-02-24 22:51:19 +0630

    Parsing csv file and transforming into the js object

--- 05_palnet_project/index.js
+++ 05_palnet_project/index.js
@@ -1,15 +1,20 @@
-const parse = require('csv-parse')
+const {parse} = require('csv-parse')
 const fs = require('fs')
+
 
 const stream = [];
 fs.createReadStream('./kepler_data.csv')
-	.on('data', chunk => {
-		stream.push(chunk)
+	.pipe(parse({
+		comment: '#',
+		columns: true
+	}))
+	.on('data', data => {
+		stream.push(data)
 	})
 	.on('error', err => {
 		console.log(err)
 	})
 	.on('close', () => {
 		console.log('Done')
-		console.log(stream.toString())
+		console.log(stream)
 	})
