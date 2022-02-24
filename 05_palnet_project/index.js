0cd59a4417d0cc3130dde65ec210b98b1e56e9f7
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-02-24 22:40:34 +0630

    Using readStream function

--- 05_palnet_project/index.js
+++ 05_palnet_project/index.js
@@ -1,1 +1,15 @@
-const parser = require('csv-parse')
+const parse = require('csv-parse')
+const fs = require('fs')
+
+const stream = [];
+fs.createReadStream('./kepler_data.csv')
+	.on('data', chunk => {
+		stream.push(chunk)
+	})
+	.on('error', err => {
+		console.log(err)
+	})
+	.on('close', () => {
+		console.log('Done')
+		console.log(stream.toString())
+	})
