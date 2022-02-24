467f1e46016de19f82a8fa8d8a49c11ce5fc6876
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-02-24 23:49:59 +0630

    Http APIs and Routing

--- 06_web_server/app.js
+++ 06_web_server/app.js
@@ -3,12 +3,28 @@ const http = require('http')
 const PORT = 3000
 
 const server = http.createServer((req, res) => {
+	const path = req.url;
+
 	res.writeHead(200, {
-		'Content-Type': 'Application/json'
+		'Content-Type': 'application/json'
 	})
-	res.end(JSON.stringify({
-		message: "Hello World!"
-	}))
+	if (path === '/messages') {
+		res.statusCode = 200;
+		res.write(JSON.stringify([
+			{message: 'Hello World'},
+			{message: 'Hi World'},
+		]))
+		res.end()
+	} else if (path === '/') {
+		res.end(JSON.stringify({
+			message: 'Welcome'
+		}))
+	} else {
+		res.statusCode = 404
+		res.end(JSON.stringify({
+			message: 'Not found'
+		}))
+	}
 })
 
 server.listen(PORT, () => {
