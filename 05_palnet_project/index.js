d573dfa0e32336b464ee497d7dd7a86a32bbc822
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-02-24 23:04:59 +0630

    Finding habitable planets

--- 05_palnet_project/index.js
+++ 05_palnet_project/index.js
@@ -2,19 +2,29 @@ const {parse} = require('csv-parse')
 const fs = require('fs')
 
 
-const stream = [];
+const habitablePlantnets = [];
+
+function isHabitable(planet) {
+	return planet['koi_disposition'] === 'CONFIRMED'
+		&& planet['koi_insol'] > 0.36
+		&& planet['koi_insol'] < 1.11
+		&& planet['koi_prad'] < 1.6
+}
+
 fs.createReadStream('./kepler_data.csv')
 	.pipe(parse({
 		comment: '#',
 		columns: true
 	}))
 	.on('data', data => {
-		stream.push(data)
+		if (isHabitable(data)) {
+			habitablePlantnets.push(data)
+		}
 	})
 	.on('error', err => {
 		console.log(err)
 	})
 	.on('close', () => {
 		console.log('Done')
-		console.log(stream)
+		console.log(habitablePlantnets.length)
 	})
