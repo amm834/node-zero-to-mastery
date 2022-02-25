25b601b2d93107af081fb585bd959dde22d89053
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-02-25 23:57:25 +0630

    Route paramenters

--- 07_express_project/app.js
+++ 07_express_project/app.js
@@ -1,13 +1,40 @@
 const express = require('express')
 
+const app = express()
+
 const PORT = 3000
 
-const app = express();
+const friends = [
+    {
+        id: 1,
+        name: 'Mg Mg',
+    },
+    {
+        id: 2,
+        name: 'Aung Aung',
+    },
+]
 
 app.get('/', (req, res) => {
-	res.send('Hello World')
+    res.send('Hello World')
+})
+
+app.get('/friends', (req, res) => {
+    res.json(friends)
+})
+
+app.get('/friends/:id', (req, res) => {
+    const id = req.params.id
+    const friend = friends[id]
+    if (friend) {
+        res.json(friend)
+    } else {
+        res.status(404).json({
+            error: 'Friend not found',
+        })
+    }
 })
 
 app.listen(PORT, () => {
-	console.log('Serever is running at port', PORT)
+    console.log('Serever is running at port', PORT)
 })
