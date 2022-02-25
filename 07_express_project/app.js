b2caea3388fcd57c855923090461025f8f49a85d
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-02-26 01:56:14 +0630

    Using hbs templating engine

--- 07_express_project/app.js
+++ 07_express_project/app.js
@@ -5,6 +5,10 @@ const path = require('path')
 
 const PORT = 3000
 const app = express()
+
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'views'))
+
 app.use('/site', express.static(path.join(__dirname, 'public')))
 app.use(
     bodyParser.urlencoded({
@@ -19,6 +23,13 @@ app.use((req, res, next) => {
 })
 
 app.use(friendsRouter)
+
+app.get('/', (req, res) => {
+    return res.render('index', {
+        caption: 'My lovely girl',
+        title: 'Title',
+    })
+})
 
 app.listen(PORT, () => {
     console.log('Serever is running at port', PORT)
