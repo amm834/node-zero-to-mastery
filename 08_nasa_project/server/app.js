36f1a362d46634b9e11b985040e21ced7dc2cb10
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-03-01 23:46:07 +0630

    Get all planets

--- 08_nasa_project/server/app.js
+++ 08_nasa_project/server/app.js
@@ -1,16 +1,21 @@
 const express = require('express')
+const cors = require('cors')
 const bodyParser = require('body-parser')
 
-const app = express()
+const planetRouter = require('./src/routes/planets.router')
 
+const app = express()
+app.use(cors())
+app.use(express.json())
 app.use(
     bodyParser.urlencoded({
         extended: true,
     })
 )
 
+app.use(planetRouter)
 app.get('/', (req, res) => {
-    res.send('hello')
+    return res.end('hi')
 })
 
 module.exports = app
--- 08_nasa_project/server/server.js
+++ 08_nasa_project/server/server.js
@@ -1,7 +1,8 @@
 require('dotenv/config')
+
 const http = require('http')
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8000
 
 const app = require('./app')
 const server = http.createServer(app)
