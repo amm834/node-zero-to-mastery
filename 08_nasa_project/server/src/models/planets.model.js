4ab175798a76885e4513865883f1579c87f6aa3e
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-03-02 00:15:43 +0630

    Set real data to the planets model

--- 08_nasa_project/server/src/models/planets.model.js
+++ 08_nasa_project/server/src/models/planets.model.js
@@ -1,3 +1,46 @@
-const planets = []
+const path = require('path')
+const { parse } = require('csv-parse')
+const fs = require('fs')
 
-module.exports = planets
+const habitablePlantnets = []
+
+function isHabitable(planet) {
+    return (
+        planet['koi_disposition'] === 'CONFIRMED' &&
+        planet['koi_insol'] > 0.36 &&
+        planet['koi_insol'] < 1.11 &&
+        planet['koi_prad'] < 1.6
+    )
+}
+
+const dataPath = path.join(__dirname, '../../data/kepler_data.csv')
+fs.createReadStream(dataPath)
+    .pipe(
+        parse({
+            comment: '#',
+            columns: true,
+        })
+    )
+    .on('data', (data) => {
+        if (isHabitable(data)) {
+            habitablePlantnets.push(data)
+        }
+    })
+    .on('error', (err) => {
+        console.log(err)
+    })
+    .on('close', () => {
+        console.log(
+            habitablePlantnets.map((planet) => {
+                return planet['kepler_name']
+            })
+        )
+        console.log(
+            'Total number of habitable planets',
+            habitablePlantnets.length
+        )
+    })
+
+module.exports = {
+    planets: habitablePlantnets,
+}
