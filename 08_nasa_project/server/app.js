5d70b04798fc90a7db5dd2da2c461a4671cd6254
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-03-01 23:58:44 +0630

    Set the cors to the express

--- 08_nasa_project/server/app.js
+++ 08_nasa_project/server/app.js
@@ -5,7 +5,11 @@ const bodyParser = require('body-parser')
 const planetRouter = require('./src/routes/planets.router')
 
 const app = express()
-app.use(cors())
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+    })
+)
 app.use(express.json())
 app.use(
     bodyParser.urlencoded({
