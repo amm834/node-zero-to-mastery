859a37e710caee48c60fb8256f81af6a6d1f6954
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-03-04 23:01:52 +0630

    Adding new launch model in client

--- 08_nasa_project/server/src/models/launches.model.js
+++ 08_nasa_project/server/src/models/launches.model.js
@@ -7,7 +7,7 @@ const launch = {
     mission: 'Kepler Exploration X',
     rocket: 'Exploser IS1',
     launcheDate: new Date('December 30,2030'),
-    destination: 'Kepler 442 B',
+    target: 'Kepler 442 B',
     customer: ['Myanmar', 'NASA'],
     upcoming: true,
     success: true,
