d25c57b237184d7055f513de5ee1320e2d28ce9c
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-03-08 02:44:35 +0630

    Using ssl and tls

---
+++ 011_node_security_example/server.js
@@ -0,0 +1,17 @@
+const http = require('https')
+const express = require('express')
+const fs = require('fs')
+
+const app = express()
+// TO generate new ssl key: openssl req -x509 -newkey rsa:4096 -nodes -keyout key.pem -out cert.pem -days 365
+app.get('/', (req, res) => {
+    res.send('<h1>Hello</h1>')
+})
+
+http.createServer(
+    {
+        key: fs.readFileSync('key.pem'),
+        cert: fs.readFileSync('cert.pem'),
+    },
+    app
+).listen(3000)
